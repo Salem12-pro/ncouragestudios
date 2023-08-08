8aec86550eaa342d6557c138a468e0658c203088
salem <muhammedsalem992@gmail.com>
2023-08-08 04:07:30 -0700

    salem

---
+++ main.js
@@ -0,0 +1,10 @@
+var menudiv = document.querySelector(".menu")
+var showlink = document.querySelector(".mobilemaster")
+
+
+
+
+function dosomething() {
+    menudiv.classList.toggle("show-burger")
+    showlink.classList.toggle("show-link")
+}
